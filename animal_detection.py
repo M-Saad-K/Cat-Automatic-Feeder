cd0e3985c890d53a889d89658749f5315dc98beb
M-Saad-K <mskscotland@gmail.com>
2026-08-26 14:05:17 +0100

    Adding detection model

--- animal_detection.py
+++ animal_detection.py
@@ -17,3 +17,14 @@ classNames = []
 classFile = "/home/saadkhan1/Desktop/Object_Detection_Files/coco.names"
 with open(classFile,"rt") as f:
     classNames = f.read().rstrip("\n").split("\n")
+
+configPath = "/home/saadkhan1/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/saadkhan1/Desktop/Object_Detection_Files/frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
